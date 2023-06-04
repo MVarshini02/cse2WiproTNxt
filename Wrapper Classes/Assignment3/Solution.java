0553160961864981bc805af32a69eae94522bba6
MVarshini02 <101333542+MVarshini02@users.noreply.github.com>
2023-06-04 18:31:48 +0530

    I/O streams and wraper classes

---
+++ Wrapper Classes/Assignment3/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the integer number (1 to 255) :");
+		int intNum = scan.nextInt();
+		
+		String output = String.format("%8s", Integer.toBinaryString(intNum)).replace(' ', '0');
+		System.out.println(output);	
+	}
+
+}
